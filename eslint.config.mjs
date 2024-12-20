836df6a9be6cf6ad1e832ae24198b8a160729f73
seriouslegend2 <kaushaledara22@gmail.com>
2024-12-21 01:46:41 +0530

    Co-authored-by: Abhiram Kothagundu <AbhiramKothagundu@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // Warn instead of error and ignore variables starting with "_"
+    },
+  },
 ];
 
 export default eslintConfig;
